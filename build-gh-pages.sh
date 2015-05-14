78748e4890025e2e1150bf3e6105e6054d5a8498
Kris Kowal <kris.kowal@cixar.com>
2015-05-13 21:08:26 -0700

    Simplify gh-pages build script

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -6,22 +6,18 @@ export PATH="$HERE/node_modules/.bin":"$PATH"
 export GIT_DIR="$HERE/.git"
 export GIT_INDEX_FILE=$(mktemp "$GIT_DIR/TEMP.XXXXXX")
 
-function gentree1() {
-    JS_BUNDLE=$(git hash-object -w <(mrs index.js))
-    echo "100644 blob $JS_BUNDLE"$'\t'"bundle.js"
-    HTML_BUNDLE=$(git hash-object -w bundle.html)
-    echo "100644 blob $HTML_BUNDLE"$'\t'"index.html"
-    CSS_BUNDLE=$(git hash-object -w <(lessc index.less))
-    echo "100644 blob $CSS_BUNDLE"$'\t'"index.css"
+function gentree() {
+    echo "100644 blob $(git hash-object -w <(mrs index.js))"$'\t'"bundle.js"
+    echo "100644 blob $(git hash-object -w bundle.html)"$'\t'"index.html"
+    echo "100644 blob $(git hash-object -w <(lessc index.less))"$'\t'"index.css"
 }
 
-OVERLAY=$(gentree1 | git mktree)
+OVERLAY=$(gentree | git mktree)
 git read-tree --empty
-git read-tree --prefix=/ "$OVERLAY"
+git read-tree --prefix=/ $OVERLAY
 TREE=$(git write-tree --missing-ok)
 PARENT=$(git rev-parse refs/heads/master)
-COMMIT=$(git commit-tree -p "$PARENT" "$TREE" < <(echo "Create bundles"))
-
+COMMIT=$(git commit-tree -p $PARENT $TREE < <(echo Create bundles))
 git update-ref refs/heads/gh-pages $COMMIT
 
 rm $GIT_INDEX_FILE
